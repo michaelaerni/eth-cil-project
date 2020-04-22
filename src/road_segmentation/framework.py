7d83a0ff3bae0939e9e0c3d14c27e257eb788cab
Michael Aerni <michael.aerni@hotmail.com>
2020-04-22 18:52:29 +0200

    Adjusted scores to ensure they are correct

--- src/road_segmentation/framework.py
+++ src/road_segmentation/framework.py
@@ -346,7 +346,7 @@ class KerasHelper(object):
 
         return [
             _BinaryMeanFScore(threshold=threshold),
-            _BinaryMeanIoU(threshold=threshold)
+            _BinaryMeanIoUScore(threshold=threshold)
         ]
 
     class _LogPredictionsCallback(tf.keras.callbacks.LambdaCallback):
@@ -399,19 +399,91 @@ class KerasHelper(object):
 
 
 # FIXME: Could move metrics into separate module
-# TODO: Metrics currently treat a collection of images as one single big image. It is very likely that this is not the intended way.
-class _BinaryMeanIoU(tf.keras.metrics.MeanIoU):
-    def __init__(self, name: str = 'binary_mean_iou', threshold: float = 0.5, dtype=None):
-        super(_BinaryMeanIoU, self).__init__(num_classes=2, name=name, dtype=dtype)
+class _BinaryMeanIoUScore(tf.keras.metrics.Metric):
+    def __init__(self, name: str = 'binary_iou_score', threshold: float = 0.5, dtype=None):
+        super(_BinaryMeanIoUScore, self).__init__(name=name, dtype=dtype)
         self._threshold = threshold
 
-    def update_state(self, y_true, y_pred, sample_weight=None):
-        # Determine classes by using threshold
-        y_true = tf.cast(y_true > self._threshold, y_true.dtype)
-        y_pred = tf.cast(y_pred > self._threshold, y_pred.dtype)
+        # Add a variable to collection confusion matrices
+        self._score_accumulator = self.add_weight(
+            name='score_accumulator',
+            shape=(),
+            initializer=tf.initializers.zeros,
+            dtype=self._dtype
+        )
+        self._sample_counter = self.add_weight(
+            name='sample_counter',
+            shape=(),
+            initializer=tf.initializers.zeros,
+            dtype=tf.int64
+        )
 
-        # Pass to actual metric
-        return super(_BinaryMeanIoU, self).update_state(y_true, y_pred, sample_weight)
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        # Cast and threshold labels
+        y_true = tf.cast(y_true > self._threshold, self._dtype)
+        y_pred = tf.cast(y_pred > self._threshold, self._dtype)
+
+        # Fix input shapes to be (batch size, num predictions)
+        y_true = self._fix_shapes(y_true)
+        y_pred = self._fix_shapes(y_pred)
+
+        # First dimension is assumed to be batch
+        batch_size = tf.cast(tf.shape(y_true)[0], tf.int64)
+
+        true_positives = tf.reduce_sum(
+            tf.cast((y_pred == y_true) & (y_pred == 1), self._dtype),
+            axis=1
+        )
+        false_positives = tf.reduce_sum(
+            tf.cast((y_pred != y_true) & (y_pred == 1), self._dtype),
+            axis=1
+        )
+        false_negatives = tf.reduce_sum(
+            tf.cast((y_pred != y_true) & (y_pred == 0), self._dtype),
+            axis=1
+        )
+
+        # Calculate batch-wise score
+        scores = tf.math.divide_no_nan(
+            true_positives,
+            true_positives + false_positives + false_negatives
+        )
+
+        # Update counter with observed number of samples
+        self._sample_counter.assign_add(batch_size)
+
+        # Finally update sum accumulator
+        return self._score_accumulator.assign_add(
+            tf.reduce_sum(scores)
+        )
+
+    def result(self):
+        # Safely handle cases with zero observed samples
+        result = tf.math.divide_no_nan(
+            self._score_accumulator,
+            tf.cast(self._sample_counter, self._dtype)
+        )
+
+        return result
+
+    def reset_states(self):
+        # Reset score sum and sample counter to zero
+        tf.keras.backend.set_value(self._score_accumulator, 0.0)
+        tf.keras.backend.set_value(self._sample_counter, 0)
+
+    @classmethod
+    def _fix_shapes(cls, values: tf.Tensor) -> tf.Tensor:
+        if values.shape.ndims == 1:
+            # Expand dimensions if non-batched
+            # Assumes values is are the predictions for a single sample
+            values = tf.expand_dims(values, axis=0)
+
+        if values.shape.ndims > 2:
+            # Flatten samples
+            # Assumes first dimension is batch size, rest is predictions
+            values = tf.reshape(values, (tf.shape(values)[0], -1))
+
+        return values
 
 
 class _BinaryMeanFScore(tf.keras.metrics.Metric):
@@ -420,11 +492,17 @@ class _BinaryMeanFScore(tf.keras.metrics.Metric):
         self._threshold = threshold
 
         # Add a variable to collection confusion matrices
-        self._confusion_accumulator = self.add_weight(
-            name='confusion_accumulation',
-            shape=(2, 2),
+        self._score_accumulator = self.add_weight(
+            name='score_accumulator',
+            shape=(),
             initializer=tf.initializers.zeros,
-            dtype=tf.float64  # Needs to be int64 for confusion matrix
+            dtype=self._dtype
+        )
+        self._sample_counter = self.add_weight(
+            name='sample_counter',
+            shape=(),
+            initializer=tf.initializers.zeros,
+            dtype=tf.int64
         )
 
     def update_state(self, y_true, y_pred, sample_weight=None):
@@ -432,38 +510,67 @@ class _BinaryMeanFScore(tf.keras.metrics.Metric):
         y_true = tf.cast(y_true > self._threshold, self._dtype)
         y_pred = tf.cast(y_pred > self._threshold, self._dtype)
 
-        # Flatten labels is necessary
-        if y_pred.shape.ndims > 1:
-            y_pred = tf.reshape(y_pred, (-1,))
-        if y_true.shape.ndims > 1:
-            y_true = tf.reshape(y_true, (-1,))
+        # Fix input shapes to be (batch size, num predictions)
+        y_true = self._fix_shapes(y_true)
+        y_pred = self._fix_shapes(y_pred)
 
-        # Calculate confusion matrix over current samples
-        current_confusion = tf.math.confusion_matrix(
-            y_true,
-            y_pred,
-            num_classes=2,
-            weights=sample_weight,
-            dtype=tf.float64
+        # First dimension is assumed to be batch
+        batch_size = tf.cast(tf.shape(y_true)[0], tf.int64)
+
+        true_positives = tf.reduce_sum(
+            tf.cast((y_pred == y_true) & (y_pred == 1), self._dtype),
+            axis=1
+        )
+        false_positives = tf.reduce_sum(
+            tf.cast((y_pred != y_true) & (y_pred == 1), self._dtype),
+            axis=1
+        )
+        false_negatives = tf.reduce_sum(
+            tf.cast((y_pred != y_true) & (y_pred == 0), self._dtype),
+            axis=1
         )
 
-        # Add confusion matrix to accumulator
-        return self._confusion_accumulator.assign_add(current_confusion)
+        # Calculate batch-wise score
+        scores = tf.math.divide_no_nan(
+            2.0 * true_positives,
+            2.0 * true_positives + false_positives + false_negatives
+        )
+
+        # Update counter with observed number of samples
+        self._sample_counter.assign_add(batch_size)
+
+        # Finally update sum accumulator
+        return self._score_accumulator.assign_add(
+            tf.reduce_sum(scores)
+        )
 
     def result(self):
-        true_positives = self._confusion_accumulator[1, 1]
-        false_negatives = self._confusion_accumulator[1, 0]
-        false_positives = self._confusion_accumulator[0, 1]
-
-        # Safely handle cases in which the denominator is zero
-        return tf.math.divide_no_nan(
-            2.0 * true_positives,
-            (2.0 * true_positives + false_negatives + false_positives)
+        # Safely handle cases with zero observed samples
+        result = tf.math.divide_no_nan(
+            self._score_accumulator,
+            tf.cast(self._sample_counter, self._dtype)
         )
 
+        return result
+
     def reset_states(self):
-        # Set accumulator to zero again
-        tf.keras.backend.set_value(self._confusion_accumulator, np.zeros((2, 2)))
+        # Reset score sum and sample counter to zero
+        tf.keras.backend.set_value(self._score_accumulator, 0.0)
+        tf.keras.backend.set_value(self._sample_counter, 0)
+
+    @classmethod
+    def _fix_shapes(cls, values: tf.Tensor) -> tf.Tensor:
+        if values.shape.ndims == 1:
+            # Expand dimensions if non-batched
+            # Assumes values is are the predictions for a single sample
+            values = tf.expand_dims(values, axis=0)
+
+        if values.shape.ndims > 2:
+            # Flatten samples
+            # Assumes first dimension is batch size, rest is predictions
+            values = tf.reshape(values, (tf.shape(values)[0], -1))
+
+        return values
 
 
 def _setup_logging(debug: bool):
