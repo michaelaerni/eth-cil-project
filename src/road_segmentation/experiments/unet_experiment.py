9da41938aef72ed3febd85294fee62688e67a13e
Nicolas Hoferer <developer.n.h@gmail.com>
2020-05-05 18:47:00 +0200

    Add data augmentation according to U-Net paper

--- src/road_segmentation/experiments/unet_experiment.py
+++ src/road_segmentation/experiments/unet_experiment.py
@@ -1,11 +1,123 @@
 import argparse
 import typing
 import numpy as np
+
 import road_segmentation as rs
 import tensorflow as tf
 
+import tensorflow_addons as tfa
+
 EXPERIMENT_DESCRIPTION = 'U-Net Baseline'
 EXPERIMENT_TAG = 'baseline_unet'
+
+
+@tf.function
+def randomly_shift_image_and_mask(image: tf.Tensor, mask: tf.Tensor) -> typing.Tuple[tf.Tensor, tf.Tensor]:
+    """
+    Randomly shifts an image and its mask.
+    Padding is done via 'REFLECT' strategy, i.e. mirroring
+
+    The shift is chosen randomly.
+    Args:
+        image: image which should be shifted
+        mask: mask which should be shifted
+
+    Returns:
+        shifted image and mask
+    """
+    original_height = tf.shape(image)[0]
+    original_width = tf.shape(image)[1]
+    height_padding = original_height // 4
+    width_padding = original_width // 4
+    paddings = [(height_padding, height_padding), (width_padding, width_padding), (0, 0)]
+    image = tf.pad(image, paddings, mode="REFLECT")
+    mask = tf.pad(mask, paddings, mode="REFLECT")
+
+    height_offset = tf.random.uniform([], 0, image.shape[0] - original_height, dtype=tf.int32)
+    width_offset = tf.random.uniform([], 0, image.shape[1] - original_width, dtype=tf.int32)
+
+    image = tf.image.crop_to_bounding_box(image, height_offset, width_offset, original_height, original_width)
+    mask = tf.image.crop_to_bounding_box(mask, height_offset, width_offset, original_height, original_width)
+
+    return image, mask
+
+
+@tf.function
+def randomly_rotate_image_and_mask(image: tf.Tensor, mask: tf.Tensor) -> typing.Tuple[tf.Tensor, tf.Tensor]:
+    """
+    Randomly rotates image and mask.
+    Padding is done via 'REFLECT' strategy, i.e. mirroring
+
+    The angle is uniformly drawn.
+
+    Args:
+        image: image which should be rotated
+        mask: mask which should be rotated by same angle as image
+
+    Returns:
+        rotated image and mask
+    """
+    original_height = tf.shape(image)[0]
+    original_width = tf.shape(image)[1]
+    paddings = [(original_height - 1, original_height - 1), (original_width - 1, original_width - 1), (0, 0)]
+    image = tf.pad(image, paddings, mode="REFLECT")
+    mask = tf.pad(mask, paddings, mode="REFLECT")
+    random_angles = tf.random.uniform(shape=(), minval=0, maxval=359)
+    image = tf.image.resize_with_crop_or_pad(tfa.image.rotate(image, random_angles, interpolation='BILINEAR'),
+                                             original_height, original_width)
+    mask = tf.image.resize_with_crop_or_pad(tfa.image.rotate(mask, random_angles, interpolation='BILINEAR'),
+                                            original_height, original_width)
+    return image, mask
+
+
+@tf.function
+def randomly_adjust_image_brightness(image: tf.Tensor,
+                                     start_interval: float = -0.2,
+                                     end_interval: float = 0.3) -> tf.Tensor:
+    """
+    Randomly adjust brightness of image by delta from uniform interval between start_interval and end_interval.
+    Args:
+        image: image which brightness should be adjusted
+        start_interval: start of interval to draw delta from
+        end_interval: end of interval to draw delta from
+
+    Returns:
+        image where brightness is adjusted
+    """
+    delta = tf.random.uniform([], start_interval, end_interval)
+    image = tf.image.adjust_brightness(image, delta)
+    return image
+
+
+@tf.function
+def randomly_augment_image_and_mask(data: typing.Tuple[tf.Tensor, tf.Tensor]) -> typing.Tuple[tf.Tensor, tf.Tensor]:
+    """
+    Randomly augments an image and a mask as follows, where each transformation is done with probability of 1/2:
+        - rotate image and mask by a random angle
+        - shift image and mask
+        - adjust brightness
+    Args:
+        data: tuple which contains image and its mask
+
+    Returns:
+        augmented image and mask
+    """
+    image = data[0]
+    mask = data[1]
+
+    if tf.random.uniform([], minval=0, maxval=1) > 0.5:
+        image, mask = randomly_rotate_image_and_mask(image, mask)
+    if tf.random.uniform([], minval=0, maxval=1) > 0.5:
+        image, mask = randomly_shift_image_and_mask(image, mask)
+    if tf.random.uniform([], minval=0, maxval=1) > 0.5:
+        image = randomly_adjust_image_brightness(image)
+
+    return image, mask
+
+
+@tf.function
+def randomly_augment_data(images, masks):
+    return tf.map_fn(randomly_augment_image_and_mask, (images, masks))
 
 
 class BaselineUnetExperiment(rs.framework.Experiment):
@@ -24,10 +136,16 @@ class BaselineUnetExperiment(rs.framework.Experiment):
         parser.add_argument('--learning-rate', type=float, default=0.01, help='Learning rate')
         parser.add_argument('--momentum', type=float, default=0.99, help='Momentum')
         parser.add_argument('--epochs', type=int, default=100, help='Number of training epochs')
-        parser.add_argument('--apply-dropout-after-conv_blocks', type=bool, default=False,
+        parser.add_argument('--apply-dropout', type=bool, default=False,
                             help='Whether or not dropout is applied after conv blocks')
-        parser.add_argument('--upsampling_method', type=str, default='transpose',
-                            help='Either "upsampling" = Upsampling2D or "transpose" = Conv2DTranspose is used in the expansive path')
+        parser.add_argument('--apply-batch-norm', type=bool, default=False,
+                            help='Whether or not batch normalization is applied')
+        parser.add_argument('--upsampling-method', type=str, default='transpose',
+                            help='"upsampling" for upsampling via interpolation or "transpose" for learnable upsampling'),
+        parser.add_argument('--number-of-filters', type=int, default=64,
+                            help='Number of filters at first downsampling block'),
+        parser.add_argument('--number-of-scaling-steps', type=int, default=4,
+                            help='Number of down and upsampling steps')
 
         return parser
 
@@ -38,8 +156,11 @@ class BaselineUnetExperiment(rs.framework.Experiment):
             'learning_rate': args.learning_rate,
             'momentum': args.momentum,
             'epochs': args.epochs,
-            'apply_dropout_after_conv_blocks': args.apply_dropout_after_conv_blocks,
-            'upsampling_method': args.upsampling_method
+            'apply_dropout': args.apply_dropout,
+            'apply_batch_norm': args.apply_batch_norm,
+            'upsampling_method': args.upsampling_method,
+            'number_of_filters': args.number_of_filters,
+            'number_of_scaling_steps': args.number_of_scaling_steps
         }
 
     def fit(self) -> typing.Any:
@@ -58,10 +179,10 @@ class BaselineUnetExperiment(rs.framework.Experiment):
         except (OSError, ValueError):
             self.log.exception('Unable to load data')
             return
-
         training_dataset = tf.data.Dataset.from_tensor_slices((training_images, training_masks))
         training_dataset = training_dataset.shuffle(buffer_size=1024)
         training_dataset = training_dataset.batch(batch_size)
+        training_dataset = training_dataset.map(randomly_augment_data)
         self.log.debug('Training data specification: %s', training_dataset.element_spec)
 
         validation_dataset = tf.data.Dataset.from_tensor_slices((validation_images, validation_masks))
@@ -71,8 +192,13 @@ class BaselineUnetExperiment(rs.framework.Experiment):
         self.log.info('Building model')
         model = rs.models.unet.UNet(
             dropout_rate=self.parameters['dropout_rate'],
-            apply_dropout_after_conv_blocks=self.parameters['apply_dropout_after_conv_blocks'],
-            upsampling_method=self.parameters['upsampling_method']
+            apply_dropout=self.parameters['apply_dropout'],
+            upsampling_method=self.parameters['upsampling_method'],
+            number_of_filters=self.parameters['number_of_filters'],
+            number_of_scaling_steps=self.parameters['number_of_scaling_steps'],
+            apply_batch_norm=self.parameters['apply_batch_norm'],
+            input_padding=((0, 0), (94, 94), (94, 94), (0, 0)),
+            output_cropping=((2, 2), (2, 2))
         )
         sgd_optimizer = tf.keras.optimizers.SGD(
             momentum=self.parameters['momentum'],
@@ -101,6 +227,8 @@ class BaselineUnetExperiment(rs.framework.Experiment):
             validation_data=validation_dataset,
             callbacks=callbacks
         )
+        tf.keras.utils.plot_model(model, expand_nested=True)
+        model.summary()
 
         self.log.info('Classifier fitted')
 
@@ -123,8 +251,8 @@ class BaselineUnetExperiment(rs.framework.Experiment):
 
             result[sample_id] = prediction_mask_patches[0]
 
-            target_height = image.shape[0] // rs.data.cil.PATCH_SIZE
-            target_width = image.shape[1] // rs.data.cil.PATCH_SIZE
+            target_height = image.shape[1] // rs.data.cil.PATCH_SIZE
+            target_width = image.shape[2] // rs.data.cil.PATCH_SIZE
 
             assert result[sample_id].shape == (target_height, target_width)
 
--- src/road_segmentation/models/unet.py
+++ src/road_segmentation/models/unet.py
@@ -1,4 +1,6 @@
+import numpy as np
 import tensorflow as tf
+import typing
 
 
 class UNet(tf.keras.Model):
@@ -6,67 +8,108 @@ class UNet(tf.keras.Model):
     Implementation of plain U-Net according to original paper (https://arxiv.org/abs/1505.04597).
     """
 
-    def __init__(self, dropout_rate: float, apply_dropout_after_conv_blocks: bool, upsampling_method: str):
+    def __init__(self,
+                 dropout_rate: float,
+                 apply_dropout: bool,
+                 upsampling_method: str,
+                 number_of_filters: int,
+                 number_of_scaling_steps: int,
+                 apply_batch_norm: bool,
+                 input_padding: typing.Tuple[
+                     typing.Tuple[int, int],
+                     typing.Tuple[int, int],
+                     typing.Tuple[int, int],
+                     typing.Tuple[int, int]],
+                 output_cropping: typing.Tuple[
+                     typing.Tuple[int, int],
+                     typing.Tuple[int, int]]):
         super(UNet, self).__init__()
+
         after_conv_block_dropout_rate = None
-        if apply_dropout_after_conv_blocks:
+        if apply_dropout:
             after_conv_block_dropout_rate = dropout_rate
-        self.contracting_path = [
-            conv_block(filters=64,
-                       size=3,
-                       stride=1,
-                       dropout_rate=after_conv_block_dropout_rate,
-                       name="down_block_1"),
-            tf.keras.Sequential([tf.keras.layers.MaxPool2D((2, 2), (2, 2))], name="max_pool_1"),
-            conv_block(128, 3, 1, dropout_rate=after_conv_block_dropout_rate, name="down_block_2"),
-            tf.keras.Sequential([tf.keras.layers.MaxPool2D((2, 2), (2, 2))], name="max_pool_2"),
-            conv_block(256, 3, 1, dropout_rate=after_conv_block_dropout_rate, name="down_block_3"),
-            tf.keras.Sequential([tf.keras.layers.MaxPool2D((2, 2), (2, 2))], name="max_pool_3"),
-            conv_block(512, 3, 1, dropout_rate=dropout_rate, name="down_block_4"),
-            tf.keras.Sequential([tf.keras.layers.MaxPool2D((2, 2), (2, 2))], name="max_pool_4"),
-        ]
 
-        self.bottleneck = conv_block(1024, 3, 1, dropout_rate=dropout_rate, name="bottleneck")
+        self.number_of_scaling_steps = number_of_scaling_steps
 
-        self.expansive_path = [
-            upsample(filters=512, size=2, dropout_rate=after_conv_block_dropout_rate,
-                     upsampling_method=upsampling_method,
-                     name="up_conv_1"),
-            conv_block(512, 3, 1, dropout_rate=after_conv_block_dropout_rate, name="conv_block_right_1"),
-            upsample(256, 2, dropout_rate=after_conv_block_dropout_rate, upsampling_method=upsampling_method,
-                     name="up_conv_2"),
-            conv_block(256, 3, 1, dropout_rate=after_conv_block_dropout_rate, name="conv_block_right_2"),
-            upsample(128, 2, dropout_rate=after_conv_block_dropout_rate, upsampling_method=upsampling_method,
-                     name="up_conv_3"),
-            conv_block(128, 3, 1, dropout_rate=after_conv_block_dropout_rate, name="conv_block_right_3"),
-            upsample(64, 2, dropout_rate=after_conv_block_dropout_rate, upsampling_method=upsampling_method,
-                     name="up_conv_4"),
-            conv_block(64, 3, 1, dropout_rate=after_conv_block_dropout_rate, name="conv_block_right_4"),
-        ]
+        self.input_padding = lambda x: tf.pad(x, input_padding, mode="REFLECT")
 
-        self.conv_out = tf.keras.layers.Conv2D(1, 1, strides=1, padding='same', activation=None)
-        self.crop_output = tf.keras.layers.Cropping2D(((2, 2), (2, 2)))
+        self.contracting_path = []
+        for i in range(self.number_of_scaling_steps):
+            current_number_of_filters = np.power(2, i) * number_of_filters
+            print(current_number_of_filters)
+            conv_block_ = conv_block(filters=current_number_of_filters,
+                                     size=(3, 3),
+                                     stride=(1, 1),
+                                     apply_batch_norm=apply_batch_norm,
+                                     dropout_rate=after_conv_block_dropout_rate,
+                                     name=f"down_block_{i + 1}")
+            self.contracting_path.append(conv_block_)
+            self.contracting_path.append(tf.keras.layers.MaxPool2D((2, 2), (2, 2), name=f"max_pool_{i + 1}"))
+
+        current_number_of_filters = np.power(2, self.number_of_scaling_steps) * number_of_filters
+
+        self.bottleneck = conv_block(current_number_of_filters,
+                                     size=(3, 3),
+                                     stride=(1, 1),
+                                     apply_batch_norm=apply_batch_norm,
+                                     dropout_rate=dropout_rate,
+                                     name="bottleneck")
+
+        self.expansive_path = []
+
+        for i in range(1, self.number_of_scaling_steps + 1):
+            current_number_of_filters = np.power(2, self.number_of_scaling_steps - i) * number_of_filters
+            print(current_number_of_filters)
+            upsampling_block = upsample(filters=current_number_of_filters,
+                                        size=(2, 2),
+                                        stride=(2, 2),
+                                        apply_batch_norm=apply_batch_norm,
+                                        dropout_rate=after_conv_block_dropout_rate,
+                                        upsampling_method=upsampling_method,
+                                        name=f"up_conv_{i}")
+            conv_block_ = conv_block(filters=current_number_of_filters,
+                                     size=(3, 3),
+                                     stride=(1, 1),
+                                     apply_batch_norm=apply_batch_norm,
+                                     dropout_rate=after_conv_block_dropout_rate,
+                                     name=f"conv_block_right_{i}")
+            self.expansive_path.append(upsampling_block)
+            self.expansive_path.append(conv_block_)
+
+        self.conv_out = tf.keras.layers.Conv2D(filters=1,
+                                               kernel_size=(1, 1),
+                                               strides=(1, 1),
+                                               padding='same',
+                                               activation=None)
+        self.crop_output = tf.keras.layers.Cropping2D(output_cropping)
 
     def call(self, inputs, training=None, mask=None):
-        x = tf.pad(inputs, ((0, 0), (94, 94), (94, 94), (0, 0)), mode="SYMMETRIC")
+        x = self.input_padding(inputs)
 
         # Downsampling and saving outputs to establish skip connection
         skips = []
-        for block in self.contracting_path:
+        for i, block in enumerate(self.contracting_path):
             x = block(x)
-            if "max_pool" not in block.name and len(skips) < 4:
+            if i % 2 == 0 and len(skips) < self.number_of_scaling_steps:
+                # if "max_pool" not in block.name and len(skips) < self.number_of_scaling_steps:
                 skips.append(x)
+
+            assert (i % 2 == 0 and len(skips) < self.number_of_scaling_steps) == (
+                    "max_pool" not in block.name and len(skips) < self.number_of_scaling_steps)
         skips = list(reversed(skips))
 
         x = self.bottleneck(x)
 
         # Upsampling, cropping and concatenation with skip connections
         counter = 0
-        for block in self.expansive_path:
+        for i, block in enumerate(self.expansive_path):
             x = block(x)
-            if "up_conv" in block.name and counter < 4:
-                concat = tf.keras.layers.Concatenate()
-                x = concat([crop_to_fit(x, skips[counter]), x])
+
+            assert ("up_conv" in block.name and counter < self.number_of_scaling_steps) == (
+                    i % 2 == 0 and counter < self.number_of_scaling_steps)
+            if i % 2 == 0 and counter < self.number_of_scaling_steps:
+                # if "up_conv" in block.name and counter < self.number_of_scaling_steps:
+                x = tf.concat([crop_to_fit(x, skips[counter]), x], axis=-1)
                 counter += 1
 
         # convert to output
@@ -81,7 +124,8 @@ def crop_to_fit(target_tensor, to_crop):
     Crops an image to width and height of target tensor.
     Args:
         target_tensor: tensor with desired shape [batch, new_height, new_width, channels]
-        to_crop: tensor which should be cropped to shape of target_tensor, shape is [batch, old_height, old_width, channels]
+        to_crop: tensor which should be cropped to shape of target_tensor,
+                 shape is [batch, old_height, old_width, channels]
 
     Returns:
         Cropped tensor of shape [batch, new_height, new_width, channels]
@@ -91,28 +135,35 @@ def crop_to_fit(target_tensor, to_crop):
 
 
 def conv_block(filters: int,
-               size,
-               stride=(1, 1),
-               use_batch_norm: bool = True,
-               dropout_rate=None,
-               name: str = None) -> tf.keras.Sequential:
+               size: typing.Tuple[int, int],
+               stride: typing.Tuple[int, int] = (1, 1),
+               apply_batch_norm: bool = False,
+               dropout_rate: float = None,
+               name: str = None) -> tf.keras.Model:
     """
-    Conv2D => ReLu => Conv2D => ReLu => (Dropout)
+    Conv2D => (BN) => ReLu => Conv2D => (BN) => ReLu => (Dropout)
 
-    :param filters: number of filters
-    :param size: size of ilters
-    :param stride: stride for conv2D
-    :param use_batch_norm: if True batch normalization is applied after each conv layer
-    :param dropout_rate: either None (no dropout) or float between 0 and 1 (apply dropout)
-    :param name: optional a name for the conv block
-    :return: conv block as sequential model
+    Args:
+        filters: number of filters
+        size: size of ilters
+        stride: stride for Conv2D
+        apply_batch_norm: if True batch normalization is applied after each conv layer
+        dropout_rate: either None (no dropout) or float between 0 and 1 (apply dropout)
+        name: optional a name for the conv block
+
+    Returns:
+        convolution block as keras model
     """
     result = tf.keras.Sequential(name=name)
     for i in range(2):
-        result.add(tf.keras.layers.Conv2D(filters, size, strides=stride, padding='valid',
-                                          kernel_initializer='he_normal', use_bias=True))
+        result.add(tf.keras.layers.Conv2D(filters,
+                                          size,
+                                          strides=stride,
+                                          padding='valid',
+                                          kernel_initializer='he_normal',
+                                          use_bias=True))
 
-        if use_batch_norm:
+        if apply_batch_norm:
             result.add(tf.keras.layers.BatchNormalization())
 
         result.add(tf.keras.layers.ReLU())
@@ -122,40 +173,50 @@ def conv_block(filters: int,
     return result
 
 
-def upsample(filters,
-             size,
-             stride=(2, 2),
-             use_batch_norm: bool = True,
+def upsample(filters: int,
+             size: typing.Tuple[int, int],
+             stride: typing.Tuple[int, int] = (2, 2),
+             apply_batch_norm: bool = False,
              name: str = None,
-             dropout_rate=None,
-             upsampling_method: str = 'transpose') -> tf.keras.Sequential:
+             dropout_rate: float = None,
+             upsampling_method: str = 'transpose') -> tf.keras.Model:
     """
-    Conv2DTranspose => (BatchNorm) => ReLu
+    Conv2DTranspose => (BatchNorm) => ReLu => (Dropout)
+    or
+    UpSampling2D => Conv2D => (BatchNorm) => ReLu => (Dropout)
 
-    :param filters: number of filters
-    :param size: size of filters
-    :param stride: stride for conv2D
-    :param use_batch_norm: if True batch normalization is applied after each conv layer
-    :param name: optional a name for the upsampling block
-    :param dropout_rate: either None (no dropout) or float between 0 and 1 (apply dropout)
-    :param upsampling_method: either "upsampling" = Upsampling2D or "transpose" = Conv2DTranspose in the expansive path
-    :return: up sample block as sequential model
+    Args:
+        filters: number of filters
+        size: size of filters
+        stride: stride for Conv2D
+        apply_batch_norm: if True batch normalization is applied after each conv layer
+        name: optional a name for the upsampling block
+        dropout_rate: either None (no dropout) or float between 0 and 1 (apply dropout)
+        upsampling_method: "upsampling" for upsampling via interpolation or "transpose" for learnable upsampling
+
+    Returns:
+        up sample block
     """
     result = tf.keras.Sequential(name=name)
     if upsampling_method == 'upsampling':
         result.add(tf.keras.layers.UpSampling2D(size=(2, 2)))
         result.add(
-            tf.keras.layers.Conv2D(filters, 2, activation='relu', padding='same', kernel_initializer='he_normal'))
+            tf.keras.layers.Conv2D(filters,
+                                   kernel_size=(2, 2),
+                                   activation='relu',
+                                   padding='same',
+                                   kernel_initializer='he_normal'))
     elif upsampling_method == 'transpose':
         result.add(
-            tf.keras.layers.Conv2DTranspose(filters, size,
+            tf.keras.layers.Conv2DTranspose(filters,
+                                            size,
                                             strides=stride,
                                             padding='same',
                                             kernel_initializer='he_normal',
                                             use_bias=False))
     else:
         raise ValueError("Unknown upsampling_method: {}".format(upsampling_method))
-    if use_batch_norm:
+    if apply_batch_norm:
         result.add(tf.keras.layers.BatchNormalization())
     if dropout_rate:
         result.add(tf.keras.layers.Dropout(dropout_rate))
