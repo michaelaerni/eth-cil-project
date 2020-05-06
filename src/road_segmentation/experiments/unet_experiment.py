db903d8d5f85d21c1ecdb4845875ad35ad7225fa
Michael Aerni <michael.aerni@hotmail.com>
2020-05-06 22:00:39 +0200

    Included re-implementation of U-Net in actual experiment. Since the data augmentation was broken (i.e. the library used is not in the conda environment and at the moment not compatible) it had to be rewritten too

--- src/road_segmentation/experiments/unet_experiment.py
+++ src/road_segmentation/experiments/unet_experiment.py
@@ -1,126 +1,18 @@
 import argparse
 import typing
-import numpy as np
 
-import road_segmentation as rs
+import numpy as np
 import tensorflow as tf
 
-import tensorflow_addons as tfa
+import road_segmentation as rs
 
 EXPERIMENT_DESCRIPTION = 'U-Net Baseline'
 EXPERIMENT_TAG = 'baseline_unet'
 
-INPUT_PADDING = ((0, 0), (94, 94), (94, 94), (0, 0))
-OUTPUT_CROPPING = ((2, 2), (2, 2))
+INPUT_PADDING = ((94, 94), (94, 94))
 
 
-@tf.function
-def randomly_shift_image_and_mask(image: tf.Tensor, mask: tf.Tensor) -> typing.Tuple[tf.Tensor, tf.Tensor]:
-    """
-    Randomly shifts an image and its mask.
-    Padding is done via 'REFLECT' strategy, i.e. mirroring
-
-    The shift is chosen randomly.
-    Args:
-        image: image which should be shifted
-        mask: mask which should be shifted
-
-    Returns:
-        shifted image and mask
-    """
-    original_height = tf.shape(image)[0]
-    original_width = tf.shape(image)[1]
-    height_padding = original_height // 4
-    width_padding = original_width // 4
-    paddings = [(height_padding, height_padding), (width_padding, width_padding), (0, 0)]
-    image = tf.pad(image, paddings, mode="REFLECT")
-    mask = tf.pad(mask, paddings, mode="REFLECT")
-
-    height_offset = tf.random.uniform([], 0, image.shape[0] - original_height, dtype=tf.int32)
-    width_offset = tf.random.uniform([], 0, image.shape[1] - original_width, dtype=tf.int32)
-
-    image = tf.image.crop_to_bounding_box(image, height_offset, width_offset, original_height, original_width)
-    mask = tf.image.crop_to_bounding_box(mask, height_offset, width_offset, original_height, original_width)
-
-    return image, mask
-
-
-@tf.function
-def randomly_rotate_image_and_mask(image: tf.Tensor, mask: tf.Tensor) -> typing.Tuple[tf.Tensor, tf.Tensor]:
-    """
-    Randomly rotates image and mask.
-    Padding is done via 'REFLECT' strategy, i.e. mirroring
-
-    The angle is uniformly drawn.
-
-    Args:
-        image: image which should be rotated
-        mask: mask which should be rotated by same angle as image
-
-    Returns:
-        rotated image and mask
-    """
-    original_height = tf.shape(image)[0]
-    original_width = tf.shape(image)[1]
-    paddings = [(original_height - 1, original_height - 1), (original_width - 1, original_width - 1), (0, 0)]
-    image = tf.pad(image, paddings, mode="REFLECT")
-    mask = tf.pad(mask, paddings, mode="REFLECT")
-    random_angles = tf.random.uniform(shape=(), minval=0, maxval=359)
-    image = tf.image.resize_with_crop_or_pad(tfa.image.rotate(image, random_angles, interpolation='BILINEAR'),
-                                             original_height, original_width)
-    mask = tf.image.resize_with_crop_or_pad(tfa.image.rotate(mask, random_angles, interpolation='BILINEAR'),
-                                            original_height, original_width)
-    return image, mask
-
-
-@tf.function
-def randomly_adjust_image_brightness(image: tf.Tensor,
-                                     start_interval: float = -0.2,
-                                     end_interval: float = 0.3) -> tf.Tensor:
-    """
-    Randomly adjust brightness of image by delta from uniform interval between start_interval and end_interval.
-    Args:
-        image: image which brightness should be adjusted
-        start_interval: start of interval to draw delta from
-        end_interval: end of interval to draw delta from
-
-    Returns:
-        image where brightness is adjusted
-    """
-    delta = tf.random.uniform([], start_interval, end_interval)
-    image = tf.image.adjust_brightness(image, delta)
-    return image
-
-
-@tf.function
-def randomly_augment_single_image_and_mask(image: tf.Tensor, mask: tf.Tensor) \
-        -> typing.Tuple[tf.Tensor, tf.Tensor]:
-    """
-    Randomly augments an image and a mask as follows:
-        - rotate image and mask by a random angle
-        - shift image and mask
-        - adjust brightness
-    where each transformation is done with probability of 1/2
-
-    Args:
-        image: single image
-        mask: single mask
-
-    Returns:
-        augmented image and mask
-    """
-
-    if tf.random.uniform([], minval=0, maxval=1) > 0.5:
-        image, mask = randomly_rotate_image_and_mask(image, mask)
-    if tf.random.uniform([], minval=0, maxval=1) > 0.5:
-        image, mask = randomly_shift_image_and_mask(image, mask)
-    if tf.random.uniform([], minval=0, maxval=1) > 0.5:
-        image = randomly_adjust_image_brightness(image)
-
-    return image, mask
-
-
-class BaselineUnetExperiment(rs.framework.Experiment):
+class BaselineUNetExperiment(rs.framework.Experiment):
 
     @property
     def tag(self) -> str:
@@ -132,35 +24,24 @@ class BaselineUnetExperiment(rs.framework.Experiment):
 
     def create_argument_parser(self, parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
         parser.add_argument('--batch-size', type=int, default=1, help='Training batch size')
-        parser.add_argument('--dropout-rate', type=float, default=0.5, help='Dropout rate')
         parser.add_argument('--learning-rate', type=float, default=0.01, help='Learning rate')
         parser.add_argument('--momentum', type=float, default=0.99, help='Momentum')
         parser.add_argument('--epochs', type=int, default=100, help='Number of training epochs')
-        parser.add_argument('--apply-dropout', type=bool, default=False,
-                            help='Whether or not dropout is applied after conv blocks')
-        parser.add_argument('--apply-batch-norm', type=bool, default=False,
+        parser.add_argument('--dropout-rate', type=float, default=None, help='Dropout rate')
+        parser.add_argument('--apply-batch-norm', action='store_true',
                             help='Whether or not batch normalization is applied')
-        parser.add_argument('--upsampling-method', type=str, default='transpose',
-                            help='"upsampling" for upsampling via interpolation or "transpose" for learnable upsampling'),
-        parser.add_argument('--number-of-filters-at-start', type=int, default=64,
-                            help='Number of filters at first downsampling block'),
-        parser.add_argument('--number-of-scaling-steps', type=int, default=4,
-                            help='Number of down and upsampling steps')
-
         return parser
 
     def build_parameter_dict(self, args: argparse.Namespace) -> typing.Dict[str, typing.Any]:
         return {
             'batch_size': args.batch_size,
-            'dropout_rate': args.dropout_rate,
             'learning_rate': args.learning_rate,
             'momentum': args.momentum,
             'epochs': args.epochs,
-            'apply_dropout': args.apply_dropout,
+            'dropout_rate': args.dropout_rate,
             'apply_batch_norm': args.apply_batch_norm,
-            'upsampling_method': args.upsampling_method,
-            'number_of_filters_at_start': args.number_of_filters_at_start,
-            'number_of_scaling_steps': args.number_of_scaling_steps
+            'augmentation_max_brightness_delta': 0.2,
+            'augmentation_max_shift': 20
         }
 
     def fit(self) -> typing.Any:
@@ -181,7 +62,15 @@ class BaselineUnetExperiment(rs.framework.Experiment):
             return
         training_dataset = tf.data.Dataset.from_tensor_slices((training_images, training_masks))
         training_dataset = training_dataset.shuffle(buffer_size=1024)
-        training_dataset = training_dataset.map(randomly_augment_single_image_and_mask)
+        # Apply data augmentation to training data
+        training_dataset = training_dataset.map(
+            lambda image, mask: augment_sample(
+                image,
+                mask,
+                self.parameters['augmentation_max_brightness_delta'],
+                self.parameters['augmentation_max_shift']
+            )
+        )
         training_dataset = training_dataset.batch(batch_size)
 
         self.log.debug('Training data specification: %s', training_dataset.element_spec)
@@ -191,20 +80,10 @@ class BaselineUnetExperiment(rs.framework.Experiment):
 
         # Build model
         self.log.info('Building model')
-
-        # FIXME: (if applied to other dataset)
-        # INPUT_PADDING and OUTPUT_CROPPING are constants that work exactly for the given training and test data,
-        # it will NOT necessarly work for data with a different shape
-
         model = rs.models.unet.UNet(
-            dropout_rate=self.parameters['dropout_rate'],
-            apply_dropout=self.parameters['apply_dropout'],
-            upsampling_method=self.parameters['upsampling_method'],
-            number_of_filters_at_start=self.parameters['number_of_filters_at_start'],
-            number_of_scaling_steps=self.parameters['number_of_scaling_steps'],
-            apply_batch_norm=self.parameters['apply_batch_norm'],
             input_padding=INPUT_PADDING,
-            output_cropping=OUTPUT_CROPPING
+            apply_batch_norm=self.parameters['apply_batch_norm'],
+            dropout_rate=self.parameters['dropout_rate']
         )
         sgd_optimizer = tf.keras.optimizers.SGD(
             momentum=self.parameters['momentum'],
@@ -233,8 +112,6 @@ class BaselineUnetExperiment(rs.framework.Experiment):
             validation_data=validation_dataset,
             callbacks=callbacks
         )
-        tf.keras.utils.plot_model(model, expand_nested=True)
-        model.summary()
 
         self.log.info('Classifier fitted')
 
@@ -250,6 +127,7 @@ class BaselineUnetExperiment(rs.framework.Experiment):
 
             image = np.asarray([image])
             raw_predictions = classifier.predict(image)
+            # FIXME: Sigmoid here is unnecessary
             raw_predictions = tf.sigmoid(raw_predictions)
             prediction_mask = np.where(raw_predictions >= PREDICTION_THRESHOLD, 1, 0)
 
@@ -265,8 +143,54 @@ class BaselineUnetExperiment(rs.framework.Experiment):
         return result
 
 
+def augment_sample(
+        image: tf.Tensor,
+        mask: tf.Tensor,
+        max_brightness_delta: float,
+        max_shift: int
+) -> typing.Tuple[tf.Tensor, tf.Tensor]:
+    """
+    Randomly augments a single input sample (image and mask combination) via random brightness shift,
+    random flips and random shift followed by a crop.
+
+    Args:
+        image: 3D RGB image to augment with shape (H, W, 3).
+        mask: Corresponding mask to augment with shape (H, W, 1).
+        max_brightness_delta: Maximum brightness offset in [0, 1].
+        max_shift: Maximum shift of image (with reflected padding).
+
+    Returns:
+        Augmented sample as a tuple (image, mask) with same shapes and semantics as inputs.
+    """
+
+    # Randomly shift brightness
+    brightness_shifted = tf.image.random_brightness(image, max_delta=max_brightness_delta)
+
+    # Add mask as 4th channel to image to ensure all spatial transformations are equal
+    concatenated_sample = tf.concat([brightness_shifted, mask], axis=-1)
+
+    # Randomly flip
+    flipped = tf.image.random_flip_left_right(concatenated_sample)
+    flipped = tf.image.random_flip_up_down(flipped)
+
+    # Randomly crop rotated image to correct shape
+    pre_expanded_shape = tf.shape(flipped)
+    expanded = tf.pad(
+        flipped,
+        ((max_shift, max_shift), (max_shift, max_shift), (0, 0)),
+        mode='reflect'
+    )
+    cropped = tf.image.random_crop(expanded, pre_expanded_shape)
+
+    # Separate image and mask again
+    output_image = cropped[:, :, :3]
+    output_mask = cropped[:, :, 3:]
+
+    return output_image, output_mask
+
+
 def main():
-    BaselineUnetExperiment().run()
+    BaselineUNetExperiment().run()
 
 
 if __name__ == '__main__':
