a8f8905e4a011435c1c6197d4db41431507ec168
Michael Aerni <michael.aerni@hotmail.com>
2020-05-18 20:09:21 +0200

    Mostly finished ResNet backbone

--- src/road_segmentation/models/resnet.py
+++ src/road_segmentation/models/resnet.py
@@ -44,7 +44,9 @@ class ResNetBackbone(tf.keras.Model):
             ResNetLayer(
                 current_blocks,
                 self._INITIAL_FILTERS * (2 ** idx),
-                downsample=(idx > 0)  # No downsampling on first ResNet block due to the initial max pooling
+                downsample=(idx > 0),  # No downsampling on first ResNet block due to the initial max pooling
+                kernel_initializer=self._KERNEL_INITIALIZER,
+                weight_decay=weight_decay
             )
             for idx, current_blocks in enumerate(blocks)
         ]
@@ -100,14 +102,29 @@ class ResNetLayer(tf.keras.layers.Layer):
             blocks: int,
             initial_features: int,
             downsample: bool,
+            kernel_initializer: str,
+            weight_decay: float,
             **kwargs
     ):
         super(ResNetLayer, self).__init__(**kwargs)
 
-        pass  # TODO
+        self.blocks = [
+            BottleneckBlock(
+                initial_features,
+                downsample=(downsample and idx == 0),  # Downsampling always on first block (if desired)
+                projection_shortcut=(idx == 0),  # Projection shortcut always on first block
+                kernel_initializer=kernel_initializer,
+                weight_decay=weight_decay
+            )
+            for idx in range(blocks)
+        ]
 
     def call(self, inputs, **kwargs):
-        pass  # TODO
+        features = inputs
+        for current_block in self.blocks:
+            features = current_block(features)
+
+        return features
 
 
 class BottleneckBlock(tf.keras.layers.Layer):
@@ -119,14 +136,17 @@ class BottleneckBlock(tf.keras.layers.Layer):
             self,
             filters_in: int,
             downsample: bool,
+            projection_shortcut: bool,
             kernel_initializer: str,
             weight_decay: float,
             **kwargs
     ):
         super(BottleneckBlock, self).__init__(**kwargs)
 
+        # Number of filters grows by factor of 4
+        filters_out = 4 * filters_in
+
         # Initial 1x1 convolution
-        # FIXME: The FastFCN paper implements downsampling differently, the original ResNet does as follows
         strides_in = (2, 2) if downsample else (1, 1)
         self.conv_in = tf.keras.layers.Conv2D(
             filters=filters_in,
@@ -154,7 +174,7 @@ class BottleneckBlock(tf.keras.layers.Layer):
 
         # Output 1x1 convolution without activation (is done externally) and 4x as many filters
         self.conv_out = tf.keras.layers.Conv2D(
-            filters=4 * filters_in,
+            filters=filters_out,
             kernel_size=1,
             padding='same',
             activation=None,
@@ -162,17 +182,50 @@ class BottleneckBlock(tf.keras.layers.Layer):
             kernel_regularizer=tf.keras.regularizers.l2(weight_decay)
         )
         self.batch_norm_out = tf.keras.layers.BatchNormalization()
+        self.activation_out = tf.keras.layers.ReLU()
+
+        # Projection shortcut if required (i.e. when downsampling or changing the number of features)
+        self.conv_residual = None
+        self.batch_norm_residual = None
+        if downsample or projection_shortcut:
+            self.conv_residual = tf.keras.layers.Conv2D(
+                filters=filters_out,
+                kernel_size=1,
+                strides=strides_in,
+                padding='same',
+                activation=None,
+                use_bias=False,  # No bias on projection shortcut
+                kernel_initializer=kernel_initializer,
+                kernel_regularizer=tf.keras.regularizers.l2(weight_decay)
+            )
+            self.batch_norm_residual = tf.keras.layers.BatchNormalization()
+            # No activation here, is done on all features on output
 
     def call(self, inputs, **kwargs):
-        features = self.conv_in(inputs)
-        features = self.batch_norm_in(features)
-        features1 = self.activation_in(features)
+        # Perform projection shortcut if necessary
+        residuals = inputs
+        if self.conv_residual is not None:
+            assert self.batch_norm_residual is not None
+            residuals = self.conv_residual(residuals)
+            residuals = self.batch_norm_residual(residuals)
 
-        features = self.conv_middle(features1)
-        features = self.batch_norm_middle(features)
-        features2 = self.activation_middle(features)
+        # Differentiate actual block from residual handling
+        with tf.keras.backend.name_scope('block'):
+            features = self.conv_in(inputs)
+            features = self.batch_norm_in(features)
+            features1 = self.activation_in(features)
 
-        features = self.conv_out(features2)
-        output = self.batch_norm_out(features)
+            features = self.conv_middle(features1)
+            features = self.batch_norm_middle(features)
+            features2 = self.activation_middle(features)
+
+            features = self.conv_out(features2)
+            block_output = self.batch_norm_out(features)
+
+        # Add residuals
+        pre_activation = block_output + residuals
+
+        # Perform final activation
+        output = self.activation_out(pre_activation)
 
         return output
