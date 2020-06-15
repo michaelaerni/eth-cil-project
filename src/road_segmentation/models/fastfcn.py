0d09eedb72d476aed5e72c46f3b1eea358e67a18
Michael Aerni <michael.aerni@hotmail.com>
2020-06-15 11:38:52 +0200

    Moved test semi-finished FastFCN implementation to final module

--- src/road_segmentation/models/fastfcn.py
+++ src/road_segmentation/models/fastfcn.py
@@ -3,6 +3,56 @@ import typing
 import tensorflow as tf
 
 
+class TestFastFCN(tf.keras.models.Model):
+    """
+    FIXME: This is just a test class and should be renamed and moved
+    """
+
+    KERNEL_INITIALIZER = 'he_normal'  # FIXME: This is somewhat arbitrarily chosen
+
+    def __init__(
+            self,
+            jpu_features: int,
+            weight_decay: float,
+            output_upsampling: str
+    ):
+        super(TestFastFCN, self).__init__()
+
+        self.backbone = rs.models.resnet.ResNet50Backbone(weight_decay=weight_decay)
+        self.upsampling = rs.models.fastfcn.JPUModule(
+            features=jpu_features,
+            weight_decay=weight_decay
+        )
+
+        # FIXME: Head is only for testing, replace this with EncNet head
+        self.head = rs.models.fastfcn.FCNHead(
+            intermediate_features=256,
+            kernel_initializer=self.KERNEL_INITIALIZER,
+            weight_decay=weight_decay
+        )
+
+        # FIXME: Upsampling of the 8x8 output is slightly unnecessary and should be done more in line with the s16 target
+        self.output_upsampling = tf.keras.layers.UpSampling2D(size=(8, 8), interpolation=output_upsampling)
+
+        # FIXME: The paper uses an auxiliary FCNHead at the end to calculate the loss, but never for the output...
+        #  Does not really make sense and is also not mentioned in the paper I think
+
+    def call(self, inputs, training=None, mask=None):
+        _, input_height, input_width, _ = tf.unstack(tf.shape(inputs))
+        padded_inputs = rs.util.pad_to_stride(inputs, target_stride=32, mode='REFLECT')
+
+        intermediate_features = self.backbone(padded_inputs)[-3:]
+
+        upsampled_features = self.upsampling(intermediate_features)
+
+        small_outputs = self.head(upsampled_features)
+
+        padded_outputs = self.output_upsampling(small_outputs)
+        outputs = tf.image.resize_with_crop_or_pad(padded_outputs, input_height, input_width)
+
+        return outputs
+
+
 class JPUModule(tf.keras.layers.Layer):
     """
     TODO: All documentation
