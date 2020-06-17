f0cf233e25b5ec4a8b9067f7d3f8b078ca08994d
Michael Aerni <michael.aerni@hotmail.com>
2020-06-17 14:08:31 +0200

    Added MoCo model module

---
+++ src/road_segmentation/models/moco.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+"""
+Model parts and losses for MoCo and the improved MoCo framework based on
+Momentum Contrast for Unsupervised Visual Representation Learning (arXiv:1911.05722 [cs.CV])
+and
+Improved Baselines with Momentum Contrastive Learning (arXiv:2003.04297 [cs.CV])
+"""
