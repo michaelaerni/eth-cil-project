408aa9afe69704f78fd6886fa4d69c9a04d567cd
Michael Aerni <michael.aerni@hotmail.com>
2020-06-18 17:37:16 +0200

    Fixed periodic checkpoint callback for cases without a loss available

--- src/road_segmentation/experiments/moco_representations.py
+++ src/road_segmentation/experiments/moco_representations.py
@@ -135,7 +135,7 @@ class MoCoRepresentationsExperiment(rs.framework.Experiment):
 
         callbacks = [
             self.keras.tensorboard_callback(),
-            self.keras.periodic_checkpoint_callback()
+            self.keras.periodic_checkpoint_callback(checkpoint_template='{epoch:04d}-{loss:.4f}.h5')
         ] + model.create_callbacks()  # Required MoCo updates
 
         # Fit model
