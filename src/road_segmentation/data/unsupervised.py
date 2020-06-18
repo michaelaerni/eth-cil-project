ade8b88db2e5376e4d053fcbaa130be5a324e349
Nicolas Hoferer <developer.n.h@gmail.com>
2020-06-18 08:26:37 +0200

    Adapted pipeline to not include overlap and to smaller target image size

--- src/road_segmentation/data/unsupervised.py
+++ src/road_segmentation/data/unsupervised.py
@@ -4,57 +4,64 @@ import os
 import typing
 
 import numpy as np
-from PIL import Image, ImageDraw
+from PIL import Image
 
 import road_segmentation as rs
 
 DATASET_TAG = 'unsupervised'
 
-CITIES = ["Dallas", "Detroit", "Houston", "Milwaukee"]
+CITIES = ["Boston", "Dallas", "Detroit", "Houston", "Milwaukee"]
 
 _log = logging.getLogger(__name__)
 
 
 def extract_patches_from_image(
         image: np.ndarray,
-        overlap: int,
         target_height: int,
-        target_width: int) -> typing.List[np.ndarray]:
+        target_width: int, count) -> typing.List[np.ndarray]:
     """
     Extract patches of size (target_height, target_width) from one one image such that as many patches are extracted as possible.
     """
+    #TODO remove count parameter
     _log.info('Extract Patches from images...')
-    overlapY = overlap
-    overlapX = overlap
-
     all_patches = []
     orig_image_width = image.shape[1]
     orig_image_height = image.shape[0]
 
-    flooredx = math.floor((orig_image_width - overlapX) / (target_width - overlapX))
-    flooredy = math.floor((orig_image_height - overlapY) / (target_height - overlapY))
+    num_patches_fit_width = orig_image_width / target_width
+    num_patches_fit_height = orig_image_height / target_height
 
-    maxX = math.floor((orig_image_width - overlapX) / (target_width - overlapX) - flooredx / 2 + 1)
-    maxY = math.floor((orig_image_height - overlapY) / (target_height - overlapY) - flooredy / 2 + 1)
-    minX = int(-(flooredx / 2 - 1))
-    minY = int(-(flooredy / 2 - 1))
+    maxX = math.ceil(num_patches_fit_width - num_patches_fit_width / 2) + 1
+    maxY = math.ceil(num_patches_fit_height - num_patches_fit_height / 2) + 1
+    minX = int(-(num_patches_fit_width / 2 - 1)) - 1
+    minY = int(-(num_patches_fit_height / 2 - 1)) - 1
 
-    xStart = (orig_image_width - 2 * (target_width - overlapX) - overlapX) / 2
-    yStart = (orig_image_height - 2 * (target_height - overlapY) - overlapY) / 2
-
+    xStart = (orig_image_width / 2 - target_width)
+    yStart = (orig_image_height / 2 - target_height)
+    #TODO remove
+    # pil_image = Image.fromarray(image)
     for i in range(minY, maxY):
         for j in range(minX, maxX):
-            left = xStart + (target_width - overlapX) * j
-            upper = yStart + (target_height - overlapY) * i
+            left = xStart + target_width * j
+            upper = yStart + target_height * i
             right = left + target_width
             lower = upper + target_height
 
             if np.max((upper, lower)) >= orig_image_height or np.max((left, right)) >= orig_image_width:
                 continue
+            if np.min((left, upper, right, lower)) < 0:
+                continue
+            #TODO remove
+            # draw = ImageDraw.Draw(pil_image)
+            # draw.rectangle((left, upper, right, lower), fill=50 + abs(i) * 15 + abs(j) * 25)
             all_patches.append(image[int(upper):int(lower), int(left):int(right)])
             assert np.min((left, upper, right, lower)) >= 0
             assert np.max((left, right)) < orig_image_width
             assert np.max((upper, lower)) < orig_image_height
+    # TODO remove
+    # plt.imshow(pil_image)
+    # plt.savefig("drawing_{}.png".format(count))
+    # plt.show()
 
     return all_patches
 
@@ -67,9 +74,6 @@ def save_images_to_png(images: typing.List[np.ndarray], output_file_path: str, f
         images: list of images which should be stored as .png files
         output_file_path: file path used for each image
         first_idx: index of first file written
-
-    Returns:
-
     """
     for i in range(len(images)):
         idx = i + first_idx
@@ -97,7 +101,7 @@ def raw_data_paths(data_dir: str = None) -> typing.Dict[str, typing.List[str]]:
         # Verify whether all files exist
         for image_path in paths_per_city[city]:
             if not os.path.isfile(image_path) or not os.path.exists(image_path):
-                raise FileNotFoundError(f'Sample satellite image {image_path} not found')
+                raise FileNotFoundError(f'Image {image_path} not found')
 
     return paths_per_city
 
@@ -128,7 +132,9 @@ def preprocessed_tfrecord_data_paths(data_dir: str = None) -> typing.List[str]:
     return paths
 
 
-def preprocessed_png_data_paths_per_city(data_dir: str = None) -> typing.Dict[str, typing.List[str]]:
+def preprocessed_png_data_paths_per_city(data_dir: str = None,
+                                         image_height: int = 588,
+                                         image_width: int = 588) -> typing.Dict[str, typing.List[str]]:
     """
     Returns a dictionary with image paths for each city.
     The key refers to the city and the value is a list of image paths for that city.
@@ -139,7 +145,7 @@ def preprocessed_png_data_paths_per_city(data_dir: str = None) -> typing.Dict[st
 
     image_paths = {}
     for city in CITIES:
-        city_directory = os.path.join(base_directory, city)
+        city_directory = os.path.join(base_directory, city + "{}x{}".format(image_height, image_width))
         files = os.listdir(city_directory)
         image_paths[city] = [os.path.join(city_directory, file) for file in files if file.endswith('.png')]
     return image_paths
--- src/road_segmentation/preprocess_raw_data.py
+++ src/road_segmentation/preprocess_raw_data.py
@@ -1,3 +1,4 @@
+import gc
 import logging
 import os
 import time
@@ -17,15 +18,15 @@ Afterwards the .png files get stored in a .tfrecord file per city
 The intermediate step of storing the patches as .png is done due to memory constraints.
 
 In the end for each city one .tfrecord file is produced and each contains all patches for that particular city.
-The patches are of size 1132x1132 with an overlap of 100 pixel.
+The patches are of size 588x588.
 """
 
 _log = logging.getLogger(__name__)
 
+
 def preprocess_unsupervised_data(data_dir: str = None,
-                                 target_height: int = 1132,
-                                 target_width: int = 1132,
-                                 overlap: int = 100):
+                                 target_height: int = 588,
+                                 target_width: int = 588):
     """
     Main method to run unsupervised data preprocessing
     """
@@ -33,36 +34,40 @@ def preprocess_unsupervised_data(data_dir: str = None,
     if data_dir is None:
         data_dir = rs.util.DEFAULT_DATA_DIR
 
-    paths_per_city = rs.data.unsupervised.raw_data_paths(
-        data_dir)  # get dictionary with path to each .tif image per city
+    paths_per_city = rs.data.unsupervised.raw_data_paths(data_dir)
     output_dir = os.path.join(data_dir, 'processed', "unsupervised")
-
+    count = 0
     start = time.time()
     for city in rs.data.unsupervised.CITIES:
         _log.info("Processing {}... (Takes a few minutes)".format(city))
-        output_file = os.path.join(output_dir, city + "_new")
+        output_file = os.path.join(output_dir, city + "{}x{}".format(target_height, target_width))
         if not os.path.exists(output_file):
             os.makedirs(output_file)
         start_idx = 0
         for i, image_path in enumerate(paths_per_city[city]):
-            _log.info("Image {} of {} for {}".format(i, len(paths_per_city[city]), city))
+            print("Image {} of {} for {}".format(i, len(paths_per_city[city]), city))
             image = rs.data.cil.load_image(image_path)
-            image = image[:, :, :3]  # convert_color_space(np.asarray(image))
-            patches = rs.data.unsupervised.extract_patches_from_image(image, overlap, target_height, target_width)
+            image = image[:, :, :3]
+            patches = rs.data.unsupervised.extract_patches_from_image(image, target_height, target_width, count)
+            count += 1
+            # TODO remove print statement
             print(len(patches), patches[-1].shape)
             rs.data.unsupervised.save_images_to_png(patches, output_file, start_idx)
             start_idx += len(patches)
+            if i % 10 == 0:
+                # TODO test if this really helps to avoid increasing memory
+                gc.collect()
 
-        _log.info("Number of patches for {}: {}".format(city, start_idx))
-    _log.info("Process took {} seconds".format(time.time() - start))
+        print("Number of patches for {}: {}".format(city, start_idx))
+    print("Process took {} seconds".format(time.time() - start))
 
     image_paths_per_city = rs.data.unsupervised.preprocessed_png_data_paths_per_city(data_dir)
-    _log.info("Found paths for {} cities".format(len(image_paths_per_city)))
+    print("Found paths for {} cities".format(len(image_paths_per_city)))
 
     for city in rs.data.unsupervised.CITIES:
-        _log.info("Process city: {}...".format(city))
+        print("Process city: {}...".format(city))
         output_file = os.path.join(data_dir, "processed", "unsupervised",
-                                   'new{}.tfrecord'.format(city))
+                                   '{}-{}x{}.tfrecord'.format(city, target_height, target_width))
         writer = TFRecordWriter(output_file)
         serialized_features_dataset = tf.data.Dataset.from_generator(
             rs.data.tf_record_util.image_path_generator,
@@ -72,15 +77,17 @@ def preprocess_unsupervised_data(data_dir: str = None,
         )
         writer.write(serialized_features_dataset)
 
-    _log.info("Finished preprocessing unsupervised data")
+    print("Finished preprocessing unsupervised data")
 
 
 def main():
-    data_dir = "/media/nic/VolumeAcer/CIL_data"
-    target_image_width = 1132
-    target_image_height = 1132
-    overlap = 100
-    preprocess_unsupervised_data(data_dir, target_image_height, target_image_width, overlap)
+    # TODO remove path
+    # data_dir = "/media/nic/VolumeAcer/CIL_data"
+    target_image_width = 588
+    target_image_height = 588
+    preprocess_unsupervised_data(data_dir=None,
+                                 target_image_height=target_image_height,
+                                 target_image_width=target_image_width)
 
 
 if __name__ == '__main__':
