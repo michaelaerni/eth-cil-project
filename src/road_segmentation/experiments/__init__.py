c8da335a97c0461c5d4783db16a89c2fd06b06fa
Michael Aerni <michael.aerni@hotmail.com>
2020-06-24 19:00:04 +0200

    Created new experiment for spatial pretext tasks for MoCo

--- src/road_segmentation/experiments/__init__.py
+++ src/road_segmentation/experiments/__init__.py
@@ -2,4 +2,5 @@ from . import baseline_cnn
 from . import baseline_fastfcn
 from . import common_baselines
 from . import moco_representations
+from . import moco_spatial_representations
 from . import old_baseline_logreg
