4aef4436c647d2b369b188294c84f8786f349bff
Nicolas Hoferer <developer.n.h@gmail.com>
2020-07-28 15:56:53 +0200

    Refine parameters which we search over

--- src/road_segmentation/experiments/search/baseline_fastfcn_search.py
+++ src/road_segmentation/experiments/search/baseline_fastfcn_search.py
@@ -52,25 +52,27 @@ class BaselineFastFCNSearchExperiment(rs.framework.SearchExperiment):
         }
 
     def build_search_space(self) -> ax.SearchSpace:
-        # TODO: Build full search space
-        # TODO: Many parameters are fixed even though they could be searched over
+        # FIXME: Many parameters are fixed even though they could be searched over
         initial_learning_rate_exp = ax.RangeParameter('initial_learning_rate_exp', ax.ParameterType.FLOAT, lower=-5.0, upper=0.0)
         end_learning_rate_exp = ax.FixedParameter('end_learning_rate_exp', ax.ParameterType.FLOAT, value=-8.0)
+        weight_decay = ax.RangeParameter('weight_decay', ax.ParameterType.FLOAT, lower=1e-5, upper=1e-3, log_scale=True)
+        learning_rate_decay = ax.RangeParameter('learning_rate_decay', ax.ParameterType.FLOAT, lower=np.finfo(np.float32).eps, upper=1.0, log_scale=True)
+
         parameters = [
             ax.FixedParameter('jpu_features', ax.ParameterType.INT, value=512),
             ax.FixedParameter('max_epochs', ax.ParameterType.INT, value=self.parameters['max_epochs']),
             ax.FixedParameter('batch_size', ax.ParameterType.INT, value=self.parameters['batch_size']),
             ax.FixedParameter('backbone', ax.ParameterType.STRING, value=self.parameters['backbone']),
-            ax.FixedParameter('weight_decay', ax.ParameterType.FLOAT, value=1e-4),
+            weight_decay,
             ax.RangeParameter('head_dropout', ax.ParameterType.FLOAT, lower=0.0, upper=0.5),
             ax.RangeParameter('segmentation_loss_ratio', ax.ParameterType.FLOAT, lower=0.0, upper=1.0),
             ax.FixedParameter('output_upsampling', ax.ParameterType.STRING, value='nearest'),
-            ax.FixedParameter('kernel_initializer', ax.ParameterType.STRING, value='he_normal'),  # FIXME: This might not necessarily be the best choice
+            ax.FixedParameter('kernel_initializer', ax.ParameterType.STRING, value='he_normal'),
             ax.FixedParameter('dense_initializer', ax.ParameterType.STRING, value='he_uniform'),  # Only for the dense weights in the Encoder head
             initial_learning_rate_exp,
             end_learning_rate_exp,
-            ax.FixedParameter('learning_rate_decay', ax.ParameterType.FLOAT, value=0.9),
-            ax.FixedParameter('momentum', ax.ParameterType.FLOAT, value=0.9)
+            learning_rate_decay,
+            ax.RangeParameter('momentum', ax.ParameterType.FLOAT, lower=0.8, upper=0.95, log_scale=True),
         ]
 
         parameter_constraints = []
