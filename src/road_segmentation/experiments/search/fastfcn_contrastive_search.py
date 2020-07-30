a00224642ac514ae66b3de6d93ee6e703ec3d18b
Michael Aerni <michael.aerni@hotmail.com>
2020-07-30 21:38:43 +0200

    Fixed cancer formatting for hopefully the last last last last last time

--- src/road_segmentation/experiments/search/fastfcn_contrastive_search.py
+++ src/road_segmentation/experiments/search/fastfcn_contrastive_search.py
@@ -187,13 +187,13 @@ class FastFCNContrastiveSearchExperiment(rs.framework.SearchExperiment):
         )
 
         callbacks = [
-                        self.keras.decay_temperature_callback(
-                            initial_temperature=parameterization['moco_initial_temperature'],
-                            min_temperature=parameterization['moco_min_temperature'],
-                            decay_steps=parameterization['max_epochs'],
-                            decay_rate=parameterization['moco_temperature_decay']
-                        )
-                    ] + model.create_callbacks()  # For MoCo updates
+            self.keras.decay_temperature_callback(
+                initial_temperature=parameterization['moco_initial_temperature'],
+                min_temperature=parameterization['moco_min_temperature'],
+                decay_steps=parameterization['max_epochs'],
+                decay_rate=parameterization['moco_temperature_decay']
+            )
+        ] + model.create_callbacks()  # For MoCo updates
 
         model.fit(
             training_dataset,
