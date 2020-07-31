0a8299bc1ff93b4eec9501a3eb124aef5109de40
ml-nic <developer.n.h@gmail.com>
2020-07-31 21:07:28 +0200

    Remove obsolete todo

--- src/road_segmentation/experiments/fit/fastfcn_no_se_loss.py
+++ src/road_segmentation/experiments/fit/fastfcn_no_se_loss.py
@@ -40,7 +40,6 @@ class FastFCNNoSELossExperiment(rs.framework.FitExperiment):
         return parser
 
     def build_parameter_dict(self, args: argparse.Namespace) -> typing.Dict[str, typing.Any]:
-        # TODO: Adjust after search
         return {
             'jpu_features': 512,
             'codewords': 32,
